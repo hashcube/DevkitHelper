3a4c95d378a51d8443c2e646a577b51da9afe484
Jishnu Mohan <jishnu7@gmail.com>
2016-02-09 17:53:16 +0530

    use composite to create map bridges

--- scripts/map_bridge.sh
+++ scripts/map_bridge.sh
@@ -4,20 +4,19 @@ then
     exit
 fi
 
-map_height=`identify -format "%[fx:h]" $1`
-map_width=`identify -format "%[fx:w]" $1`
+map1_height=`identify -format "%[fx:h]" $1`
+map1_width=`identify -format "%[fx:w]" $1`
 overlap_height=`identify -format "%[fx:h]" $3`
 overlap_width=`identify -format "%[fx:w]" $3`
-
-crop_start=$(($map_height-$overlap_height/2))
+overlap_middle=$((overlap_height/2))
 
 output=${4:-"output.png"}
 
-convert -append $1 $2 \
-        -page +$(($map_width/2-$overlap_width/2))+$crop_start $3 \
-        -flatten \
-        -crop ${overlap_width}x${overlap_height}+0+${crop_start} \
-        +repage \
+convert -size ${overlap_width}x${overlap_height} xc:white \
+        $2 -geometry +0+0 -composite \
+        \( $1 -crop ${overlap_width}x${overlap_middle}+0+$(($map1_height-$overlap_middle)) +repage \) \
+            -geometry +0+$overlap_middle -composite \
+        $3 -geometry +0+0 -composite \
         ${output}
 
 echo "Output:" $output
