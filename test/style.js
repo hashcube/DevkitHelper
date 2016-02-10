e3bd958a656bee3f0a3a8a31b79029374a2a831c
Jishnu Mohan <jishnu7@gmail.com>
2016-02-10 11:02:00 +0530

    test cases for style module

---
+++ test/style.js
@@ -0,0 +1,89 @@
+/* global CACHE:true, device:true, style, util_test */
+
+jsio('import test.lib.util as util_test');
+jsio('import device');
+
+var prepare = function (orientation, tablet, phablet) {
+  'use strict';
+
+  CACHE = {
+    'resources/data/config.json': JSON.stringify({
+      orientation: orientation,
+      bound_width: 100,
+      lives: {},
+      bound_height: 100
+    })
+  };
+  CACHE['resources/styles/' + orientation + '.json'] = JSON.stringify({
+    key1: {
+      prop: 'val'
+    },
+    key2: {
+      extend: 'key1',
+      prop2: 'val2'
+    }
+  });
+
+  util_test.removeFromJSIOCache('style.js');
+  device.screen.width = 500;
+  device.screen.height = 1000;
+  device.isTablet = tablet || false;
+  device.isPhablet = phablet || false;
+
+  jsio('import src.modules.style as style');
+};
+
+describe('Style:', function  () {
+  'use strict';
+
+  describe('init()', function  () {
+    it('should calculate base height and width for portrait', function () {
+      prepare('portrait');
+      assert.strictEqual(100, style.base_width);
+      assert.strictEqual(200, style.base_height);
+      assert.strictEqual(5, style.scale);
+      assert.strictEqual(2, style.scale_height);
+    });
+
+    it('should calculate base height and width for landscape', function () {
+      prepare('landscape');
+      assert.strictEqual(50, style.base_width);
+      assert.strictEqual(100, style.base_height);
+      assert.strictEqual(10, style.scale);
+      assert.strictEqual(1, style.scale_height);
+    });
+
+    it('should set tablet scale value for tablets', function () {
+      prepare('landscape', true);
+      assert.strictEqual(0.7, style.tablet_scale);
+    });
+
+    it('should set tablet scale value as 1 for mobilephones', function () {
+      prepare('landscape');
+      assert.strictEqual(1, style.tablet_scale);
+    });
+
+    it('should set tablet scale value as 0.9 for phablets', function () {
+      prepare('landscape', false, true);
+      assert.strictEqual(0.9, style.tablet_scale);
+    });
+  });
+
+  describe('get()', function  () {
+    it('should return style from json', function () {
+      prepare('portrait');
+      assert.deepEqual({prop: 'val'}, style.get('key1'));
+    });
+
+    it('should merge with passed properties', function () {
+      prepare('portrait');
+      assert.deepEqual({prop: 'val', prop3: 'val3'},
+        style.get('key1', {prop3: 'val3'}));
+    });
+
+    it('should extend from default property', function () {
+      prepare('portrait');
+      assert.deepEqual({prop: 'val', prop2: 'val2'}, style.get('key2'));
+    });
+  });
+});
