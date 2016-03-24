7626cf67cbc8a7505dabda1c1b303c54b7503e6c
rohan92 <rohan@hashcube.com>
2016-03-24 17:36:10 +0530

    Adding country code fetching

---
+++ src/country_code.js
@@ -0,0 +1,23 @@
+/* global _ */
+
+/* jshint ignore:start */
+import util.underscore as _;
+import util.ajax as ajax;
+/* jshint ignore:end */
+
+exports = (function (cb) {
+  "use strict";
+
+  // When we have multiple services this can be a array to iterate through
+  var service_url = 'http://ip-api.com/json';
+
+  ajax.get({
+    url: service_url
+  }, function (err, res) {
+    if (res && _.has(res, "countryCode")) {
+      cb(res.countryCode);
+    } else {
+      cb('UNKNOWN');
+      }
+  });
+});
