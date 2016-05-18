6ea429f50f0007e642d01b2672ca29a4c144c111
sksethi25 <sksethi25@gmail.com>
2016-05-18 09:13:13 +0530

    added fallback to country code finder

--- src/country.js
+++ src/country.js
@@ -9,15 +9,23 @@ exports = (function (cb) {
   "use strict";
 
   // When we have multiple services this can be a array to iterate through
-  var service_url = 'http://ip-api.com/json';
+  var service_url = [
+                    ['country_code', 'http://freegeoip.net/json/'],
+                    ['countryCode', 'http://ip-api.com/ejson']
+                    ],
+    it = 0,
+    ajax_req = function () {
+      ajax.get({
+        url: service_url[it][1]
+      }, function (err, res) {
+        if (res && _.has(res, service_url[it][0])) {
+          cb(res[service_url[it][0]]);
+        } else {
+          ++it;
+          ajax_req();
+        }
+      });
+    };
 
-  ajax.get({
-    url: service_url
-  }, function (err, res) {
-    if (res && _.has(res, "countryCode")) {
-      cb(res.countryCode);
-    } else {
-      cb('UNKNOWN');
-    }
-  });
+  ajax_req();
 });
